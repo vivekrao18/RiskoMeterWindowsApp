da31afdbfc11f29ab05c7ea68b067c1b883a72a2
vivekrao18 <vivekrao@uw.edu>
2014-03-20 11:39:27 -0700

    Windows App for RiskoMeter

---
+++ testROR/Navigation.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace testROR
+{
+    public partial class PanoramaPage1 : PhoneApplicationPage
+    {
+        public PanoramaPage1()
+        {
+            InitializeComponent();
+         
+        }
+
+        private void Panorama_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          //  NavigationService.Navigate(new Uri("/Navigation.xaml?goto=2", UriKind.Relative));
+            Panorama al=(Panorama)ROR.SelectedItem;
+            age.Text = al.ToString();
+        }
+
+        private void hello_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            String ageInput = hello.Text;
+           
+        }
+        
+
+       
+    }
+}
